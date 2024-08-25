9cb58a9a290f8297e009433865dc210a7f33d663
YAN, Zhiyuan <810824494@qq.com>
2024-08-25 18:40:34 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pip install numpy==1.21.5
+pip install pandas==1.4.2
+pip install Pillow==9.0.1
+pip install dlib==19.24.0
+pip install imageio==2.9.0
+pip install imgaug==0.4.0
+pip install tqdm==4.61.0
+pip install scipy==1.7.3
+pip install seaborn==0.11.2
+pip install pyyaml==6.0
+pip install imutils==0.5.4
+pip install opencv-python==4.6.0.66
+pip install scikit-image==0.19.2
+pip install scikit-learn==1.0.2
+pip install albumentations==1.1.0
+pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install efficientnet-pytorch==0.7.1
+pip install timm==0.6.12
+pip install segmentation-models-pytorch==0.3.2
+pip install torchtoolbox==0.1.8.2
+pip install tensorboard==2.10.1
+pip install setuptools==59.5.0
+pip install loralib
+pip install pytorchvideo
+pip install einops
+pip install transformers
+pip install filterpy
+pip install simplejson
+pip install kornia
+pip install fvcore
+pip install imgaug==0.4.0
+pip install git+https://github.com/openai/CLIP.git
